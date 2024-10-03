1216e26052b7a95bc7ec2936ad7e8ef8f0fc3de9
Noureddin El Adila <noureddineladila@gmail.com>
2024-10-03 17:26:06 +0200

    create project in java

---
+++ my-simple-app/src/main/java/com/example/my/simple/app/Task.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.example.my.simple.app;
+
+/**
+ *
+ * @author noure
+ */
+public class Task {
+
+    private String description;
+    private boolean isCompleted;
+
+    public Task(String description) {
+        this.description = description;
+        this.isCompleted = false;
+    }
+
+    // Getters y setters
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+
+    
+}
+
+    public void setCompleted(boolean completed) {
+        this.isCompleted = completed;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "description='" + description + '\'' +
+                ", isCompleted=" + isCompleted +
+                '}';
+    }
+}
